80a25918a766c8c19da09c23b207d4e398aed502
Andres Munevar <afmunevarm@hotmail.com>
2023-09-16 19:10:43 -0400

    Update endpoints

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from helpers.detect_people import detect_people
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
+@app.get("/detect-people-photo")
+async def detect_people_photo():
     total_people = detect_people()
     return {
         "message": "Hello, world!",
         "total_people": total_people
     }
+    
+@app.get("/detect-people-video")
+async def detect_people_video():
+    total_people = 1
+    return 1
